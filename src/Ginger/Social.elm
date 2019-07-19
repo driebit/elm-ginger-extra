25b735371416b1c6b7ba33c2d245ee189bb5321e
Elizabeth Prins <elizabeth@driebit.nl>
2019-07-19 08:57:13 +0200

    viewShareLinksWith defaultOptions

--- src/Ginger/Social.elm
+++ src/Ginger/Social.elm
@@ -1,7 +1,9 @@
 module Ginger.Social exposing
     ( Platform(..)
-    , Ingredients
-    , viewShareLinks1
+    , viewShareLinks
+    , Options
+    , defaultOptions
+    , viewShareLinksWith
     )
 
 {-| Share your page on social platforms with these links.
@@ -10,11 +12,22 @@ the platforms can scrape your page in their favourite way. Find out which ones
 you might need here: <https://megatags.co/reference/>
 
 
-# Social sharing
+# Basic sharing
+
+Just a list of links, and nothing else.
 
 @docs Platform
-@docs Ingredients
 @docs viewShareLinks
+
+
+# Sharing with extra options
+
+For when you want to add some extra images or text to your links,
+or want to adjust the text for screen reader users.
+
+@docs Options
+@docs defaultOptions
+@docs viewShareLinksWith
 
 -}
 
@@ -22,6 +35,7 @@ import Ginger.View
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import String.Interpolate exposing (interpolate)
+import Url exposing (Url)
 
 
 {-| First, determine for which social platforms you want to have share links.
@@ -34,24 +48,39 @@ type Platform
     | Weibo
 
 
-{-| To build a social share link, you need proper ingredients. The `pageUrl` is
-the full url of the page: "<https://www.yourdomain.com/yourpage>". The `pageTitle` is
-the title of your page: "Your Page". And last but not least, you can add some
-`extraHtml` to display inside of the link, like an svg icon or some text.
+{-| Your `extraHtml` will be placed inside the `a` of the share link, and your
+customized `a11yString` will be inside the visually hidden span, before the
+platform name.
 -}
-type alias Ingredients =
-    { pageUrl : String
-    , pageTitle : String
+type alias Options msg =
+    { extraHtml : Html msg
+    , a11yString : String
     }
 
 
 {-|
 
-        view =
-            viewShareLinks [ Twitter, Facebook ] <|
-                Ingredients "url" "title" Nothing
+        defaultOptions =
+            { extraHtml = text ""
+            , a11yString = "Share this page on"
+            }
 
-`viewShareLinks` will render an `ul` with `li` for every Platform:
+-}
+defaultOptions : Options msg
+defaultOptions =
+    { extraHtml = text ""
+    , a11yString = "Share this page on"
+    }
+
+
+{-| `viewShareLinks` takes a list of platforms, the full url of the page you
+want to share, and the title of that page.
+
+        view =
+            viewShareLinks [ Twitter, Facebook ]
+                yourPageUrl "Your Page Title"
+
+This function will render an `ul` with a `li` for every Platform you specify:
 
         ul []
             [ li []
@@ -62,16 +91,17 @@ type alias Ingredients =
                     ]
                     [ span [ VISUALLY-HIDDEN-STYLES ]
                         [ Share this page on PLATFORMNAME ]
-                    , Ingredients.extraHtml
                     ]
                 ]
             , li [] [...]
             , li [] [...]
             ]
 
-With `Ingredients.pageUrl` and `Ingredients.pageTitle`, the `SHARE-URL` and
-`PLATFORMNAME` are built for each social platform. The `span` inside the `a`
-makes the link accessible to screen readers. These are the `VISUALLY-HIDDEN-STYLES`:
+With the url and the title, the `SHARE-URL` and `PLATFORMNAME` are built for
+each social platform. The `span` inside the `a` makes the link accessible to
+screen readers and is invisible to sighted users, in keeping with accessibility
+(a11y) standards.
+These are the `VISUALLY-HIDDEN-STYLES`:
 
         [ style "position" "absolute"
         , style "clip" "rect(1px,1px,1px,1px)"
@@ -83,58 +113,59 @@ makes the link accessible to screen readers. These are the `VISUALLY-HIDDEN-STYL
         ]
 
 -}
-type alias Options msg =
-    { extraHtml : Html msg
-    , a11yString : String
-    }
+viewShareLinks : List Platform -> Url -> String -> Html msg
+viewShareLinks platformList pageUrl pageTitle =
+    toHtml defaultOptions platformList pageUrl pageTitle
 
 
-defaultOptions : Options msg
-defaultOptions =
-    { extraHtml = text ""
-    , a11yString = "Share this page on"
-    }
+{-|
+
+        view =
+            viewShareLinksWith
+                { defaultOptions
+                    | extraHtml = img [] []
+                    , a11yString = "Deel deze pagina op"
+                }
+                [ Facebook, LinkedIn, Twitter, Weibo ]
+                yourPageUrl
+                yourTitle
+
+This function will render an `ul` with a `li` for every Platform you specify:
+
+        ul []
+            [ li []
+                [ a
+                    [ href SHARE-URL
+                    , target "_blank"
+                    , title PLATFORMNAME
+                    ]
+                    [ span [ VISUALLY-HIDDEN-STYLES ]
+                        [ Deel deze pagina op PLATFORMNAME ]
+                    , img [] []
+                    ]
+                ]
+            , li [] [...]
+            , li [] [...]
+            ]
+
+-}
+viewShareLinksWith : Options msg -> List Platform -> Url -> String -> Html msg
+viewShareLinksWith options platformList pageUrl pageTitle =
+    toHtml options platformList pageUrl pageTitle
 
 
-type ShareLinks msg
-    = ShareLinks (Options msg) (List Platform) Ingredients
+vieww : Url -> String -> Html msg
+vieww url title =
+    viewShareLinks [ Facebook, LinkedIn, Twitter, Weibo ]
+        url
+        title
 
 
-viewShareLinks1 : List Platform -> Ingredients -> ShareLinks msg
-viewShareLinks1 platformList ingredients =
-    ShareLinks defaultOptions platformList ingredients
-
-
-test =
-    div []
-        [ viewShareLinks1 [] { pageUrl = "foo", pageTitle = "bar" }
-            |> withExtraHtml (text "baz")
-            |> withA11yString "hola"
-            |> toHtml
-        ]
-
-
-withExtraHtml : Html msg -> ShareLinks msg -> ShareLinks msg
-withExtraHtml html (ShareLinks options platformList ingredients) =
-    ShareLinks { options | extraHtml = html } platformList ingredients
-
-
-withA11yString : String -> ShareLinks msg -> ShareLinks msg
-withA11yString str (ShareLinks options platformList ingredients) =
-    ShareLinks { options | a11yString = str } platformList ingredients
-
-
-
---
-
-
-toHtml : ShareLinks msg -> Html msg
-toHtml (ShareLinks options platformList { pageUrl, pageTitle }) =
+toHtml : Options msg -> List Platform -> Url -> String -> Html msg
+toHtml options platformList pageUrl pageTitle =
     ul [] <|
-        List.map (viewShareLink options) <|
-            buildShareLinks
-                platformList
-                [ pageUrl, pageTitle ]
+        List.map (viewShareLink options << buildLink [ Url.toString pageUrl, pageTitle ])
+            platformList
 
 
 viewShareLink : Options msg -> ( String, String ) -> Html msg
@@ -160,38 +191,26 @@ viewShareLink { a11yString, extraHtml } ( platformName, shareUrl ) =
         ]
 
 
-buildShareLinks : List Platform -> List String -> List ( String, String )
-buildShareLinks platformList urlAndTitle =
-    List.map (buildLink urlAndTitle) platformList
-
-
 buildLink : List String -> Platform -> ( String, String )
 buildLink urlAndTitle platform =
-    let
-        build =
-            Tuple.mapSecond ((|>) urlAndTitle)
-    in
-    case platform of
-        Facebook ->
-            build
+    Tuple.mapSecond ((|>) urlAndTitle) <|
+        case platform of
+            Facebook ->
                 ( "Facebook"
                 , interpolate "http://www.facebook.com/sharer.php?u={0}&t={1}"
                 )
 
-        LinkedIn ->
-            build
+            LinkedIn ->
                 ( "LinkedIn"
                 , interpolate "https://www.linkedin.com/shareArticle?mini=true&url={0}&title={1}"
                 )
 
-        Twitter ->
-            build
+            Twitter ->
                 ( "Twitter"
                 , interpolate "http://twitter.com/share?text={1}&url={0}"
                 )
 
-        Weibo ->
-            build
+            Weibo ->
                 ( "Weibo"
                 , interpolate "http://service.weibo.com/share/share.php?url={0}&title={1}"
                 )
